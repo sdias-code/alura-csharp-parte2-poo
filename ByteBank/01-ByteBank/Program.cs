f203a5349bb773adbe5f1be263135ca60dc1aa7b
sdias-code <silviodias.ms@gmail.com>
2021-08-27 00:31:28 -0400

    projeto02 criado

---
+++ ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaIsabela = new ContaCorrente();
+            contaIsabela.titular = "Isabela";
+            contaIsabela.agencia = 847;
+            contaIsabela.numero = 8476270;
+
+            Console.ReadLine();
+        }
+    }
+}
